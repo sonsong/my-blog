58c538c467f99bdd95b3078be96b2cda0c0a1279
songsong <huangsongsong@newhope-data.com>
2018-11-14 18:02:08 +0800

    实现文章的修改，删除，预览以及首页的展示

--- models/t-blog.js
+++ models/t-blog.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 //初始化blog集合的约束条件
 var blogsSchema = mongoose.Schema({
+    //作者
     author: String,
+    //文章标题
     title: String,
+    //文章类型
     type: String,
+    //发表时间
     publishTime: String,
+    //更新时间
+    updateTime: String,
+    //md内容
     mdContent: String,
+    //html内容
     htmlContent: String
 })
 
--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,25 @@ const Blogs = require('../models/t-blog');
 
 //进入首页
 router.get('/', async(ctx, next) =>{
-    ctx.render('index');
+    let blogs = [];
+
+    //加载数据库的所有文章
+    await Blogs.find({}, '_id type title publishTime htmlContent', (err, docs) =>{
+        if(!err){
+            for (const item of docs) {
+                let doc = {
+                    _id: item._id.toString(),
+                    type: item.type,
+                    title: item.title,
+                    publishTime: item.publishTime,
+                    htmlContent: item.htmlContent
+                }
+
+                blogs.unshift(doc);
+            }
+        }
+    });
+    ctx.render('index', {blogs});
 });
 
 //写文章
@@ -18,23 +36,58 @@ router.get('editor', async(ctx, next) =>{
     ctx.render('user/editor');
 });
 
-//提交文章
+/** 
+*   提交文章
+    如果参数中文章id = '' 表示新增, 否则为更新
+*/
 router.post('publish', async(ctx, next) =>{
+    //所有的表单参数
+    let params = ctx.request.body;
 
-    let mdContent = ctx.request.body['editormd-markdown-doc'];
-    let htmlContent = ctx.request.body['htmlContent'];
+    //文章编码
+    let id = params['id'];
+    //文章标题
+    let title = params['title'];
+    //文章分类
+    let type = params['type'];
+    //作者
+    let author = "huangss";
+    //md格式的内容
+    let mdContent = params['mdContent'];
+    //html格式的内容
+    let htmlContent = params['htmlContent'];    
 
     //博客对象
-    let blogs = {
-        author: 'Huangss',
-        title: '关于java的那些事',
-        type: 'java',
-        publishTime: ctx.moment,
+    let blog = {
+        title,
+        type,
         mdContent,
         htmlContent
     };
 
-    await Blogs.create(blogs, (err) =>{});
+    if(id === null || id === ''){
+        //增加作者字段
+        blog.author = author;
+        //增加发表时间
+        blog.publishTime = ctx.moment;
+
+        //新增
+        await Blogs.create(blog, (err) =>{
+            if(!err){
+                console.log("新增文章成功");
+            }
+        });
+    }else{
+        //增加更新时间
+        blog.updateTime = ctx.moment;
+
+        //修改文章
+        await Blogs.updateOne({_id: id}, blog, (err) =>{
+            if(!err){
+                console.log("修改文章成功");
+            }
+        });
+    }
 
     //跳转到文章列表页面
     ctx.redirect("/artcle_list");
@@ -43,21 +96,49 @@ router.post('publish', async(ctx, next) =>{
 //进入文章列表
 router.get('artcle_list', async(ctx, next) =>{
 
-    let blogArr = [];
+    let blogs = [];
     //查询数据库中的所有文章
     await Blogs.find({}, '_id type title publishTime', (err, docs) =>{
         if(!err){
-            blogArr = docs;
+            for (const item of docs) {
+                let doc = {
+                    _id: item._id.toString(),
+                    type: item.type,
+                    title: item.title,
+                    publishTime: item.publishTime
+                }
+
+                blogs.unshift(doc);
+            }
         }
     });
 
-    ctx.render('user/artcle_list', {blogArr});
+    ctx.render('user/artcle_list', {blogs});
 });
 
-//修改指定文章
+//跳转到修改文章页面
 router.get('updateArtcle', async(ctx, next) =>{
     //解析参数
-    let id = mongoose.Types.ObjectId(ctx.query.id)
+    let id = ctx.query.id;
+    //查询到的文章信息
+    let blog = {};
+
+    //查询该文章的所有信息
+    await Blogs.findById({_id: id}, 'type title mdContent', (err, doc) =>{
+        if(!err){
+            blog = doc;
+        }
+    });
+
+    //回到编辑页面
+    ctx.render('user/editor', {type: blog.type, title: blog.title, mdContent: blog.mdContent, id});
+});
+
+//跳转到预览页面
+router.get('previewArtcle', async(ctx, next) =>{
+    //解析参数
+    let id = ctx.query.id;
+
     //查询到的文章信息
     let blog = {};
 
@@ -68,42 +149,23 @@ router.get('updateArtcle', async(ctx, next) =>{
         }
     });
 
-    //回到编辑页面
-    ctx.render('user/editor', {blog});
-});
-
-//跳转到预览页面
-router.get('preViewArtcle', async(ctx, next) =>{
-    //解析参数
-    /* let id = mongoose.Types.ObjectId(ctx.query.id.toString());
-    console.log(id); 
-    return*/
-
-    //查询到的文章信息
-    let blog = {};
-
-    //查询该文章的所有信息
-    await Blogs.findById({_id: "5bea490075e0b54b84ab53fe"}, (err, doc) =>{
-        if(!err){
-            blog = doc;
-        }
-    });
-
-    ctx.render('user/preViewArtcle', {htmlContent: blog.htmlContent});
+    ctx.render('user/preview_artcle', {htmlContent: blog.htmlContent});
 });
 
 //删除指定的文章
 router.get('delArtcle', async(ctx, next) =>{
     //获取文章编码
-    let id = mongoose.Types.ObjectId(ctx.query.id)
+    let id = ctx.query.id;
 
     //删除该文章
-    Blogs.remove({_id: id}, (err) =>{
+    await Blogs.deleteOne({_id: id}, (err) =>{
         if(!err){
-            ctx.status = 200;
-            ctx.body = {mess: '删除成功', status: 'ok'};
+            console.log("删除文章成功");
         }
-    })
+    });
+
+    //回到文章列表
+    ctx.redirect("/artcle_list");
 });
 
 module.exports = router;
